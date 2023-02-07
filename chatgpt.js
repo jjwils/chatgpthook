52a584d5944f20b3b657b0029baf55313229743c
jjwils <mail@johnwilson.me.uk>
2023-02-07 09:44:00 +0000

    abstract api key and tidy

--- chatgpt.js
+++ chatgpt.js
@@ -1,18 +1,14 @@
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
     organization: "org-wDgrw5IfdhdW6JPw8zb6neUB",
-    apiKey: "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",
+    apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-
-
 async function chatgpt(request) {
 
-    let axiosResponse = await openai.listModels();
+    //let axiosResponse = await openai.listModels();
     //console.log(axiosResponse.data);
-
-
 
 
     const completion = await openai.createCompletion({
@@ -33,22 +29,4 @@ chatgpt()
 
 
 
-
-const axios = require('axios');
-
-
-//
-// async function sendRequest(addedLines) {
-//     const response = await axios.post('https://api.openai.com/v1/engines/davinci/jobs', {
-//         prompt: `Write the implementation for the following test\n${addedLines}`,
-//         max_tokens: 2048,
-//         model: "text-davinci-002"
-//     });
-//
-//     return response.data.choices[0].text;
-// }
-//
-// module.exports = sendRequest;
-
-//require('make-runnable');
 
